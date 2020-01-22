67db7110b8314300ef6fb4944401fa27384fad6f
Alexandr Globa <globaalexandr@gmail.com>
2020-01-22 16:24:48 +0200

    AddNameOnQuale

--- PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/City.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/City.cs
@@ -16,6 +16,7 @@ namespace PandaHR.Api.DAL.Models.Entities
 
         public Guid CountryId { get; set; }
         public Country Country { get; set; }
+
         public ICollection<CompanyCity> CompanyCities { get; set; }
         public ICollection<Vacancy> Vacancies { get; set; }
     }
