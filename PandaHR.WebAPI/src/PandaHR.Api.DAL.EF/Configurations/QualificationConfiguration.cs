0fca7d9713d841775470036213c8a8a7ab1740af
SergioVak <45003961+SergioVak@users.noreply.github.com>
2020-01-23 12:55:08 +0200

    add Guid

--- PandaHR.WebAPI/src/PandaHR.Api.DAL.EF/Configurations/QualificationConfiguration.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL.EF/Configurations/QualificationConfiguration.cs
@@ -20,10 +20,17 @@ namespace PandaHR.Api.DAL.EF.Configurations
                    .HasForeignKey(q => q.QualificationId);
 
             builder.HasData(
-                new Qualification { Name = "Trainee", Value = 1, IsDeleted = false },
-                new Qualification { Name = "Junior", Value = 2, IsDeleted = false },
-                new Qualification { Name = "Middle", Value = 3, IsDeleted = false },
-                new Qualification { Name = "Senior", Value = 4, IsDeleted = false });
+                new Qualification { Name = "Trainee", Value = 1, IsDeleted = false,
+                    Id = new Guid("6015f293-a102-459b-9fa3-2ce7cc92c386")},
+
+                new Qualification { Name = "Junior", Value = 2, IsDeleted = false,
+                    Id = new Guid("6331e0ea-9df6-4e20-9bed-b18382b180bd")},
+
+                new Qualification { Name = "Middle", Value = 3, IsDeleted = false,
+                    Id = new Guid("e2e061e1-201e-41f8-8fb8-1106b00f5ae7")},
+
+                new Qualification { Name = "Senior", Value = 4, IsDeleted = false,
+                    Id = new Guid("a76428b1-aac5-410b-af4f-811c9b474997")});
         }
     }
 }
--- PandaHR.WebAPI/src/PandaHR.Api.DAL.EF/Configurations/SpecialityConfiguration.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL.EF/Configurations/SpecialityConfiguration.cs
@@ -16,11 +16,20 @@ namespace PandaHR.Api.DAL.EF.Configurations
                 .HasForeignKey(e => e.SpecialityId);
 
             builder.HasData(
-                new Speciality { Name = "Software Engineering", IsDeleted = false },
-                new Speciality { Name = "System Analysis", IsDeleted = false },
-                new Speciality { Name = "Applied Math", IsDeleted = false },
-                new Speciality { Name = "Applied Physics", IsDeleted = false },
-                new Speciality { Name = "Computer Science", IsDeleted = false });
+                new Speciality { Name = "Software Engineering", IsDeleted = false,
+                    Id = new Guid("f98a7083-825c-496a-9112-ecd375a17dcb")},
+
+                new Speciality { Name = "System Analysis", IsDeleted = false, 
+                    Id = new Guid("3cceb22e-d32f-4a29-9c49-651b258c088d")},
+
+                new Speciality { Name = "Applied Math", IsDeleted = false,
+                    Id = new Guid("0d59cea4-85f5-4107-9d0f-8fecbe6a1933")},
+
+                new Speciality { Name = "Applied Physics", IsDeleted = false,
+                    Id = new Guid("2b82ca8b-1047-4830-83d5-e1e716b4407f")},
+
+                new Speciality { Name = "Computer Science", IsDeleted = false,
+                    Id = new Guid("3a4d31f3-c8ab-4f09-8fde-684af2890d69")});
         }
     }
 }
