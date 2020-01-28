85f273e0be2aeabba59db0dbd04fabd3e13bdcba
timur.mirzaiev@gmail.com <timur.mirzaiev@gmail.com>
2020-01-28 14:10:08 +0200

    fixed names unitofwork

--- PandaHR.WebAPI/src/PandaHR.Api.DAL/IUnitOfWork.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL/IUnitOfWork.cs
@@ -5,75 +5,14 @@ using System.Text;
 
 namespace PandaHR.Api.DAL
 {
-    public class UnitOfWork : IUnitOfWork
+    public interface IUnitOfWork
     {
-        private readonly ICompanyRepository _companyRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly ISkillRepository _skillRepository;
-        private readonly IVacancyRepository _vacancyRepository;
-        private readonly ICVRepository _cvRepository;
-        private readonly IQualificationRepository _qualificationRepository;
-        private readonly ISkillRequirementRepository _skillRequirementRepository;
-
-        public UnitOfWork(IVacancyRepository vacancyRepository,
-            ICVRepository cvRepository,
-            ISkillRepository skillRepository,
-            ICompanyRepository companyRepository,
-            IUserRepository userRepository,
-            IQualificationRepository qualificationRepository,
-            ISkillRequirementRepository skillRequirementRepository)
-        {
-            _skillRepository = skillRepository;
-            _companyRepository = companyRepository;
-            _userRepository = userRepository;
-            _vacancyRepository = vacancyRepository;
-            _cvRepository = cvRepository;
-            _qualificationRepository = qualificationRepository;
-            _skillRequirementRepository = skillRequirementRepository;
-        }
-
-        public ISkillRepository Skills
-        {
-            get
-            {
-                return _skillRepository;
-            }
-        }
-
-        public ICVRepository CVs
-        {
-            get
-            {
-                return _cvRepository;
-            }
-        }
-
-        public IVacancyRepository Vacancies
-        {
-            get
-            {
-                return _vacancyRepository;
-            }
-        }
-
-        public ICompanyRepository Companies => _companyRepository;
-
-        public IUserRepository Users => _userRepository;
-
-        public IQualificationRepository Qualifications
-        {
-            get
-            {
-                return _qualificationRepository;
-            }
-        }
-
-        public ISkillRequirementRepository SkillRequirements
-        {
-            get
-            {
-                return _skillRequirementRepository;
-            }
-        }
+        ISkillRepository Skills { get; }
+        IVacancyRepository Vacancies { get; }
+        ICVRepository CVs { get; }
+        ICompanyRepository Companies { get; }
+        IUserRepository Users { get; }
+        IQualificationRepository Qualifications { get; }
+        ISkillRequirementRepository SkillRequirements { get; }
     }
 }
