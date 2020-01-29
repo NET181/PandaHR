2e650eadd561b20b3b50cdfeccdad57e5be31bbb
SergioVak <45003961+SergioVak@users.noreply.github.com>
2020-01-29 13:45:03 +0200

    add Model Experience

--- PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/SkillRequirement.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/SkillRequirement.cs
@@ -5,7 +5,6 @@ namespace PandaHR.Api.DAL.Models.Entities
     public class SkillRequirement : BaseEntity, ISoftDeletable
     {
         public float Weight { get; set; }
-        public int ExperienceMonths { get; set; }
         public bool IsDeleted { get; set; }
 
         public Guid SkillId { get; set; }
@@ -13,6 +12,9 @@ namespace PandaHR.Api.DAL.Models.Entities
 
         public Guid KnowledgeLevelId { get; set; }
         public KnowledgeLevel KnowledgeLevel { get; set; }
+
+        public Experience Experience { get; set; }
+        public Guid ExperienceId { get; set; }
 
         public Guid VacancyId { get; set; }
         public Vacancy Vacancy { get; set; }
