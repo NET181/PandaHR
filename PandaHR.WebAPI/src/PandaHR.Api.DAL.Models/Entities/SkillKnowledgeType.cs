d023ab20b9c401b340cf56c10ee14d8faec97cb1
Kirill Rudenko <redcaplan@gmail.com>
2020-01-30 14:54:19 +0200

    Fixed coupling KnowledgeLevel - SkillKnowledgeType

--- PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/SkillKnowledgeType.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL.Models/Entities/SkillKnowledgeType.cs
@@ -4,7 +4,7 @@ namespace PandaHR.Api.DAL.Models.Entities
 {
     public class SkillKnowledgeType : ISoftDeletable
     {
-        public string Name { get; set; }
+        public int Value { get; set; }
         public bool IsDeleted { get; set; }
 
         public Guid SkillTypeId { get; set; }
