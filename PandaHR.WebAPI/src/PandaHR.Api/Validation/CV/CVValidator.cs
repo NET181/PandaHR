e7adb4a104b388f122f7904bde4e8125e5d6250f
Kirill Rudenko <redcaplan@gmail.com>
2020-02-06 11:43:31 +0200

    Revert "Removed unnecessary usings in Api layer"
    This reverts commit b96426f5ce46738ac9a8d8b19467c79ad1f44a73.

--- PandaHR.WebAPI/src/PandaHR.Api/Validation/CV/CVValidator.cs
+++ PandaHR.WebAPI/src/PandaHR.Api/Validation/CV/CVValidator.cs
@@ -1,4 +1,8 @@
-﻿using FluentValidation;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
 using PandaHR.Api.Models.CV;
 
 namespace PandaHR.Api.Validation.CV
