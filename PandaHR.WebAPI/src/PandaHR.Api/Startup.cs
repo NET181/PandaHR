b96426f5ce46738ac9a8d8b19467c79ad1f44a73
Kirill Rudenko <redcaplan@gmail.com>
2020-02-06 11:40:43 +0200

    Removed unnecessary usings in Api layer

--- PandaHR.WebAPI/src/PandaHR.Api/Startup.cs
+++ PandaHR.WebAPI/src/PandaHR.Api/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
-using AutoMapper;
 using FluentValidation.AspNetCore;
 using PandaHR.Api.Common.Contracts;
 using PandaHR.Api.DependencyResolver;
