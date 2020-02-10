9c4b27f24ac46ab1a4c5ed87d8ec6cfbd2610032
Max <max.yar.eli@gmail.com>
2020-02-10 14:33:55 +0200

    Edit CV use case addition

--- PandaHR.WebAPI/src/PandaHR.Api.DAL/Repositories/Contracts/ICVRepository.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL/Repositories/Contracts/ICVRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using PandaHR.Api.DAL.Models.Entities;
 using PandaHR.Api.DAL.DTOs.CV;
+using PandaHR.Api.DAL.DTOs.SkillKnowledge;
+using PandaHR.Api.DAL.DTOs.JobExperience;
 
 namespace PandaHR.Api.DAL.Repositories.Contracts
 {
@@ -11,6 +13,11 @@ namespace PandaHR.Api.DAL.Repositories.Contracts
     {
         Task<IEnumerable<CVSummaryDTO>> GetUserCVSummaryAsync(Guid userId, int? pageSize = 10, int? page = 1);
         Task<IEnumerable<CVforSearchDTO>> GetCVsAsync(Expression<Func<CV, bool>> predicate, int? pageSize = 10, int? page = 1);
+        Task AddSkillKnowledgeIntoCVAsync(SkillKnowledgeDTO model, Guid CVId);
+        Task DeleteSkillKnowledgeFromCVAsync(Guid skillKnowledgeId);
+        Task AddJobExperienceIntoCVAsync(JobExperienceDTO model, Guid CVId);
+        Task DeleteJobExperienceFromCVAsync(Guid JobExperienceId);
         Task AddAsync(CVDTO cv);
+        Task UpdateAsync(CVDTO cv);
     }
 }
---
+++ PandaHR.WebAPI/src/PandaHR.Api/Models/SkillKnowledge/SkillKnowledgeRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PandaHR.Api.Models.SkillKnowledge
+{
+    public class SkillKnowledgeRequestModel
+    {
+        public Guid SkillId { get; set; }
+        public Guid KnowledgeLevelId { get; set; }
+        public Guid ExperienceId { get; set; }
+    }
+}
