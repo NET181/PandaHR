f179e66ba346618a127c5524829d3c22753fbd34
timur.mirzaiev@gmail.com <timur.mirzaiev@gmail.com>
2020-02-24 15:03:33 +0200

    resolved conflicts

--- PandaHR.WebAPI/src/PandaHR.Api.DAL/Repositories/Contracts/IVacancyCVFlowRepository.cs
+++ PandaHR.WebAPI/src/PandaHR.Api.DAL/Repositories/Contracts/IVacancyCVFlowRepository.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PandaHR.Api.DAL.DTOs.VacancyCVFlow;
 using PandaHR.Api.DAL.Models.Entities;
+using PandaHR.Api.DAL.Models.Entities.Enums;
 
 namespace PandaHR.Api.DAL.Repositories.Contracts
 {
